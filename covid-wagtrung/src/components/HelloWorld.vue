47ad251a9c1c79ba21aeb1ffa82e3e906dd962f0
wagtrung26 <nqt99design@gmail.com>
2022-09-27 03:42:54 +0700

    load countries with search fillter

--- covid-wagtrung/src/components/HelloWorld.vue
+++ covid-wagtrung/src/components/HelloWorld.vue
@@ -5,10 +5,15 @@
   <h1>your country {{ yourCountry.name }}, ip: {{ yourCountry.ip }}</h1>
   <button @click="loadYourCountryName">click Load your Country name</button>
 
-  <ul v-for="country in allCountries" :key="country.index">
-    <li >
-    {{ country.name }}
-
+  <form action="submit">
+    <input type="text" v-model="viewCountry" :placeholder="viewCountry" /> 
+    <button>search</button>
+  </form>
+  <ul v-for="country, index in filteredAllCountries" :key="country.index">
+    <li @click="viewCountry = country.name">
+      <span>{{ index }} - </span>
+      <img class="countryFlag" :src="country.flag" alt="">
+      {{ country.name }}
     </li>
   </ul>
 </template>
@@ -26,7 +31,17 @@ export default {
     allCountries: Array,
   },
   data() {
-    return {};
+    return {
+      viewCountry: "",
+    };
+  },
+  computed: {
+    filteredAllCountries() {
+      return this.allCountries.filter((x) => {
+        // var namex = x.name;
+        return x.name.toLowerCase().includes(this.viewCountry.toLowerCase());
+      });
+    },
   },
   methods: {
     loadYourCountryName() {
@@ -34,10 +49,21 @@ export default {
     },
   },
   created() {
-    this.loadYourCountryName()
+    this.loadYourCountryName();
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+li {
+  cursor: pointer;
+  ::hover {
+    background-color: red;
+  }
+}
+.countryFlag{
+  width: 50px;
+  height: auto;
+}
+</style>
