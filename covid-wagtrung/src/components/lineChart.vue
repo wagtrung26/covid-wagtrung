e749ff452820cbcf09b93b65cc32135009195fce
wagtrung26 <nqt99design@gmail.com>
2022-09-29 18:56:46 +0700

    world map

--- covid-wagtrung/src/components/lineChart.vue
+++ covid-wagtrung/src/components/lineChart.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>line</h1>
-     <highcharts class="hc" :options="chartOptions" ref="chart"></highcharts>
+     <highcharts  :options="chartOptions" ref="chart"></highcharts>
   </div>
 </template>
 
@@ -21,9 +21,43 @@ export default {
        chartOptions: {
         series: [
           {
-            data: [1, 2, 3]
+            name: "cases",
+            data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+          },
+          {
+            name: "deaths",
+            data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+          },
+          {
+            name: "recovered",
+            data: [916, 22064, 11742, 29851, 42490, 35282, 8121, 90434]
+          },
+        ],
+
+        title: {
+          text: "All time"
+        },
+        subtitle: {
+          text: null
+        },
+        yAxis: {
+          title: {
+            text: "people"
           }
-        ]
+        },
+        legend: {
+          layout: "vertical",
+          align: "right",
+          verticalAlign: "top"
+        },
+        plotOptions: {
+          series: {
+            label: {
+              connectorAllowed: false
+            },
+            pointStart: 2010
+          }
+        },
       }
       
     };
--- covid-wagtrung/src/views/AboutView.vue
+++ covid-wagtrung/src/views/AboutView.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="about">
-    <h1>This is an Worldwide page</h1>
+  <div>
+    <!-- START-Template -->
+    <world-chart />
+
+    <!-- END-Template -->
   </div>
 </template>
+
+<script>
+import WorldChart from '@/components/worldChart.vue';
+// import { errorToaster } from "../../shared/service/ErrorHandler.js"
+export default {
+  components: {WorldChart},
+  name: "aboutView",
+  props: {
+  
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    
+  },
+  computed: {},
+  created() {},
+  mounted() {},
+};
+</script>
+
+<style lang="scss" scoped></style>
