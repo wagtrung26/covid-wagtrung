7a1ca9cded025428dd64e8f4e12d15c8aab99ef1
wagtrung26 <nqt99design@gmail.com>
2022-11-13 14:05:40 +0700

    c

--- src/components/donutChart.vue
+++ src/components/donutChart.vue
@@ -7,6 +7,8 @@
     <!-- sto - setTimeOut -->
     <!--  cl - console.log -->
     <div class="pbar">
+      <highcharts :options="options"></highcharts>
+
       <!-- <apexchart
         width="300"
         height="500"
@@ -15,7 +17,7 @@
       ></apexchart> -->
     </div>
 
-<!-- <p> {{continentTotal}} </p> -->
+    <!-- <p> {{continentTotal}} </p> -->
 
     <!-- END-Template -->
   </div>
@@ -26,87 +28,78 @@
 export default {
   components: {},
   name: "donutChartApex",
-  props: {continentTotal:Object},
+  props: { continentTotal: Object },
   data() {
     return {
       options: {
         chart: {
-          type: "donut",
-          backgroundColor: "rgba(0,0,0,0)"
+          type: "pie",
+            // height: 450,
+          zoomBySingleTouch: true,
+          zoomType: "x",
+          // margin: [0,10,0,10],
+          // spacing: [0, 0, 0, 0],
+          backgroundColor: "rgba(0,0,0,0)",
         },
+         credits: {
+          enabled: false,
+        },
+         exporting:{
+            enabled:false
+          },
         title: {
-          // enabled: false,
           text: "",
-          align: "left",
-          style: {
-            fontSize: "18px",
-            fontWeight: "regular",
-            fontFamily: "Lucida Grande",
-            color: "#263238",
-          },
         },
-        grid: {
-          // padding: {
-          //   top: 30,
-          //   right: 0,
-          //   bottom: 0,
-          //   left: 0,
-          // },
+        tooltip: {
+          headerFormat: "",
+          // pointFormat: '<span style="color:{point.color}">\u25CF</span> <b> {point.name}</b><br/>' +
+          //     'Area (square km): <b>{point.y}</b><br/>' +
+          //     'Population density (people per square km): <b>{point.z}</b><br/>'
         },
-        legend: {
-          show: true,
-          position: "bottom",
-          horizontalAlign: "left",
-          itemMargin: {
-            horizontal: 20,
-            vertical: 5,
-          },
-        },
-        // dropShadow: {
-        //   enabled: true,
-        //   top: 0,
-        //   left: 0,
-        //   blur: 3,
-        //   opacity: 0.5,
-        // },
-        labels: ["Total Active", "Total Recovered", "Total Deaths"],
-        colors: ["#ffa700", "#17d66d", "#e73146"],
         plotOptions: {
-          // radialBar: {
-          //   dataLabels: {
-          //     name: {
-          //       fontSize: "22px",
-          //     },
-          //     value: {
-          //       fontSize: "16px",
-          //     },
-          //     total: {
-          //       show: true,
-          //       label: "Total Cases",
-          //       formatter: function (w) {
-          //         return w.globals.seriesTotals.reduce((a, b) => {
-          //           return a + b;
-          //         }, 0);
-          //       },
-          //     },
-          //   },
-          // },
-          pie: {
-            // size: 1000,
-            customScale: 1,
-            donut: {
-              size: "75%",
-              labels: {
-                show: true,
-                total: {
-                  show: true,
-                   label: "Continent's Total Cases",
-                  showAlways: true,
-                },
-              },
+     
+         pie: {
+           shadow: false,
+            center: ['50%', '50%'],
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: false
             },
+            showInLegend: true
+        }
+    },
+    
+        series: [
+          {
+            minPointSize: 10,
+            zMin: 0,
+            name: "Total",
+            size: '90%',
+        innerSize: '70%',
+            data: [
+              {
+                name: "Active cases",
+                color: "#21a1ff",
+                y: 505992,
+                z: 92,
+              },
+              {
+                name: "Recovered",
+                y: 551695,
+                color: "#44da45",
+                z: 119,
+              },
+              {
+                name: "Deaths",
+                y: 312679,
+                color: "#ec3131",
+
+                z: 121,
+              },
+            ],
           },
-        },
+        ],
       },
 
       // series: [],
@@ -114,11 +107,17 @@ export default {
   },
   methods: {
     sample() {
-      let totalCase = this.continentTotal.active
-      let totalRecover = this.continentTotal.recovered
-      let totalDeath = this.continentTotal.deaths
-      let series = [totalCase,totalRecover,totalDeath]
-      return series
+      let totalCase = this.continentTotal.active;
+      let totalRecover = this.continentTotal.recovered;
+      let totalDeath = this.continentTotal.deaths;
+      this.options.series[0].data[0].y = totalCase;
+      this.options.series[0].data[1].y = totalRecover;
+      this.options.series[0].data[2].y = totalDeath;
+    },
+  },
+  watch: {
+    continentTotal() {
+      this.sample();
     },
   },
   computed: {},
