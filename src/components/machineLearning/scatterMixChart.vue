b53bb244d40b01115b923be9726cc4749fdf0e5d
wagtrung26 <nqt99design@gmail.com>
2022-11-16 17:26:46 +0700

    ml

--- src/components/machineLearning/scatterMixChart.vue
+++ src/components/machineLearning/scatterMixChart.vue
@@ -3,7 +3,7 @@
     <!-- <button @click="load">Load</button> -->
     <div class="flex justify-start mb-6">
       <div
-        class="sm:flexCen sm:items-end sm:justify-start sm:space-x-8 sm:mt-2 mt-4 sm:space-y-0 space-y-6 flex-wrap w-full"
+        class="sm:flexCen sm:items-start sm:justify-start sm:space-x-8 sm:space-y-8 sm:mt-2 mt-4 flex-wrap w-full"
       >
         <!-- YAXIS -->
         <div class="">
@@ -34,23 +34,72 @@
           </a-select>
         </div>
         <!-- checkbox regression -->
-        <a-checkbox-group v-model:value="checkRegression">
-          <a-row>
-            <a-col :span="8">
-              <a-checkbox value="raw">Daily Raw data</a-checkbox>
-            </a-col>
-               <a-col :span="8">
-              <a-checkbox value="pol">Polynomial</a-checkbox>
-            </a-col>
-            <a-col :span="8">
-              <a-checkbox value="lin">Linear </a-checkbox>
-            </a-col>
-         
-          </a-row>
-        </a-checkbox-group>
+        <div class="">
+          <h4
+            class="text-left font-semibold tracking-wider text-slate-400/70 text-xs uppercase mb-5"
+          >
+            Visibility
+          </h4>
+          <a-checkbox-group v-model:value="checkRegression">
+            <div class="flex space-x-4">
+              <a-checkbox value="raw">Raw data</a-checkbox>
+
+              <a-checkbox value="pol">Polynomial Regression</a-checkbox>
+
+              <a-checkbox value="lin">Linear Regression</a-checkbox>
+            </div>
+          </a-checkbox-group>
+        </div>
+        
+
+<!-- Polynomial Order -->
+        <div class="c">
+          <h4
+            class="text-left font-semibold tracking-wider text-slate-400/70 text-xs uppercase mb-5"
+          >
+            Polynomial Order
+          </h4>
+          <a-slider
+            v-model:value="orderPoly"
+            :max="10"
+            :min="2"
+            :tooltip-visible="true"
+          />
+        </div>
+
+        <div class="c">
+          <h4
+            class="text-left font-semibold tracking-wider text-slate-400/70 text-xs uppercase mb-5"
+          >
+            Polynominal Precision 
+          </h4>
+          <a-slider
+            v-model:value="precPoly"
+            :max="1000"
+            :min="0"
+            :tooltip-visible="true"
+          />
+        </div>
+
+        <div class="c">
+          <h4
+            class="text-left font-semibold tracking-wider text-slate-400/70 text-xs uppercase mb-5"
+          >
+            Number of raw data
+          </h4>
+          <a-slider
+            v-model:value="numberRaw"
+            :max="1000"
+            :min="10"
+            :tooltip-visible="true"
+          />
+        </div>
       </div>
     </div>
+    <h3> Polinominal Equation: {{polyString}} </h3>
+    <h3> R2: {{polyR2}} </h3>
     <highcharts :options="chartOptions"></highcharts>
+
     <!-- <a-skeleton
       v-if="clicked"
       active
@@ -75,10 +124,15 @@ export default {
   },
   data() {
     return {
+      numberRaw:30,
+      orderPoly: 5,
+      precPoly: 5,
+      polyString:null,
+      polyR2:null,
       show: false,
       clicked: false,
       yType: "linear",
-      checkRegression: ['raw', 'pol'],
+      checkRegression: ["raw", "pol"],
       caseType: "cases",
       chartOptions: {
         chart: {
@@ -178,27 +232,20 @@ export default {
             },
           ],
         },
+         legend: {
+          enabled: false,
+          layout: "horizontal",
+          align: "right",
+          verticalAlign: "top",
+          floating: true,
+          x: -40,
+          y: 0,
+          borderWidth: 1,
+          backgroundColor: "#FFFFFF",
+          shadow: true,
+        },
 
         series: [
-          {
-            name: "Linear regression",
-            visible: false,
-            color: "#ff7600",
-            lineWidth: 3,
-            data: [],
-            marker: {
-              enabled: false,
-            },
-          },
-          {
-            name: "Polynomial regression",
-            color: "#ff008f",
-            lineWidth: 3,
-            data: [],
-            marker: {
-              enabled: false,
-            },
-          },
           {
             name: "Daily cases",
             data: [],
@@ -221,6 +268,25 @@ export default {
               },
             },
           },
+          {
+            name: "Polynomial regression",
+            color: "#ff008f",
+            lineWidth: 3,
+            data: [],
+            marker: {
+              enabled: false,
+            },
+          },
+          {
+            name: "Linear regression",
+            visible: false,
+            color: "#ff7600",
+            lineWidth: 3,
+            data: [],
+            marker: {
+              enabled: false,
+            },
+          },
         ],
       },
     };
@@ -232,7 +298,7 @@ export default {
       this.sample();
     },
     sample() {
-      let dataRaw = this.rawY;
+      let dataRaw = this.rawY.slice(-this.numberRaw);
       let data = [];
       dataRaw.forEach((i, index) => {
         let spot = [index, i];
@@ -241,28 +307,43 @@ export default {
 
       let resultLinear = regression.polynomial(data, {
         order: 1,
-        precision: 20,
+        precision: 120,
       });
       let resultPolynomial = regression.polynomial(data, {
-        order: 5,
-        precision: 20,
+        order: this.orderPoly,
+        precision: this.precPoly,
       });
       let dataLinear = [],
         dataPolynomial = [];
+
+
+        this.polyString = resultPolynomial.string
+        this.polyR2=resultPolynomial.r2
+        console.log(" something ",resultPolynomial.predict(resultPolynomial.points.length+30) )
 
       for (let i = 0; i < resultPolynomial.points.length; i++) {
         dataLinear.push(resultLinear.points[i][1]);
         dataPolynomial.push(resultPolynomial.points[i][1]);
       }
 
-      this.chartOptions.series[0].data = dataLinear;
+      this.chartOptions.series[2].data = dataLinear;
       this.chartOptions.series[1].data = dataPolynomial;
-      this.chartOptions.series[2].data = dataRaw;
+      this.chartOptions.series[0].data = dataRaw;
       this.clicked = false;
     },
   },
   computed: {},
   watch: {
+    precPoly(){
+      this.sample();
+    },
+    numberRaw(){
+      this.sample();
+    },
+   
+    orderPoly(){
+      this.sample();
+    },
     rawY() {
       this.sample();
     },
@@ -272,16 +353,24 @@ export default {
     caseType() {
       this.$emit("caseType", this.caseType);
       if (this.caseType == "cases") {
-        this.chartOptions.series[2].color = "#0093ff";
-        this.chartOptions.series[2].name = "Daily Cases";
+        this.chartOptions.series[0].color = "#0093ff";
+        this.chartOptions.series[0].name = "Daily Cases";
       } else {
-        this.chartOptions.series[2].color = "#d6172d";
-        this.chartOptions.series[2].name = "Daily Deaths";
+        this.chartOptions.series[0].color = "#d6172d";
+        this.chartOptions.series[0].name = "Daily Deaths";
       }
     },
-    checkRegression(){
-      
-    }
+    checkRegression() {
+      let k = ["raw", "pol", "lin"];
+      k.forEach((i, index) => {
+        if (this.checkRegression.includes(i)) {
+          // linear
+          this.chartOptions.series[index].visible = true;
+        } else {
+          this.chartOptions.series[index].visible = false;
+        }
+      });
+    },
   },
   created() {
     // this.sample();
